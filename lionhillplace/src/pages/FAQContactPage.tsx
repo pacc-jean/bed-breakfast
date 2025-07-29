958ccf59c1a7b8ac6d422534cd1f8c9ec55cb96e
jean luc <jeanluc.xii.iv@gmail.com>
2025-07-29 22:29:52 +0300

    id hash navigation

--- lionhillplace/src/pages/FAQContactPage.tsx
+++ lionhillplace/src/pages/FAQContactPage.tsx
@@ -1,16 +1,32 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import ContactForm from "../components/FAQ-contact/ContactForm";
 import FAQAccordion from "../components/FAQ-contact/FAQAccordion";
 
 function FAQContactPage() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const el = document.querySelector(location.hash);
+      if (el) {
+        // Give the browser a moment to render before scrolling
+        setTimeout(() => {
+          el.scrollIntoView({ behavior: "smooth" });
+        }, 100);
+      }
+    }
+  }, [location]);
+
   return (
     <main className="w-full px-4 py-12 max-w-7xl mx-auto space-y-24 mt-10">
       {/* Contact Form Section */}
-      <section>
+      <section id="contact">
         <ContactForm />
       </section>
 
       {/* FAQ Accordion Section */}
-      <section>
+      <section id="faq">
         <FAQAccordion />
       </section>
     </main>
